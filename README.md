f07fc14bcde191aaf11101bce9c2c97c742fcc3f
RajKKapadia <raajforyou@gmail.com>
2023-08-03 15:06:16 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Text to Speech using [suno/bark](https://huggingface.co/suno/bark/tree/main)
+Generate audio from a piece of text using `suno/bark` model. The audio generated can only have max 14 seconds of length.
+
+#### Resources
+* [suno/bark GitHub](https://github.com/suno-ai/bark)
+* [Voice preset](https://suno-ai.notion.site/8b8e8749ed514b0cbf3f699013548683?v=bc67cff786b04b50b3ceb756fd05f68c)
+* [Gradio](https://www.gradio.app/docs/interface)
---
+++ utils.py
@@ -0,0 +1,31 @@
+import tempfile
+import uuid
+import os
+import time
+
+from transformers import AutoProcessor, BarkModel
+from scipy.io.wavfile import write
+import torch
+import numpy as np
+
+MODEL_NAME = 'suno/bark'
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+processor = AutoProcessor.from_pretrained(pretrained_model_name_or_path=MODEL_NAME)
+model = BarkModel.from_pretrained(pretrained_model_name_or_path=MODEL_NAME)
+model.to(DEVICE)
+
+def generate_audio(text: str, preset: str) -> None:
+    inputs = processor(text, voice_preset=preset, return_tensors='pt')
+    inputs.to(DEVICE)
+    audio_array = model.generate(**inputs)
+    audio_array = audio_array.cpu().numpy().squeeze()
+    sample_rate = model.generation_config.sample_rate
+    file_id = uuid.uuid1()
+    file_path = os.path.join(
+        tempfile.gettempdir(),
+        f'{file_id}.wav'
+    )
+    write(file_path, rate=sample_rate, data=audio_array)
+    return file_path
